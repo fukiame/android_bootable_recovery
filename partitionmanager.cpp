110c9dc76c66f15a2fd06f159d57a27b547b057b
fukiame <fukiame@proton.me>
2024-04-09 10:19:03 +0700

    hdjakqkkqkq
    Change-Id: Id6d28aa2742402a4f1d5887c93c089e640482a71

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -1875,8 +1875,7 @@ void TWPartitionManager::Update_System_Details(void) {
 
 	gui_msg("update_part_details=Updating partition details...");
 	for (iter = Partitions.begin(); iter != Partitions.end(); iter++) {
-		std::string mp = (*iter)->Mount_Point;
-		LOGINFO(mp.c_str());
+		LOGINFO(((*iter)->Mount_Point).c_str());
 		(*iter)->Update_Size(true);
 		if ((*iter)->Can_Be_Mounted) {
 			if ((*iter)->Mount_Point == Get_Android_Root_Path()) {
